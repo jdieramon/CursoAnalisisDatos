7b5fa57e4107e4d3163662d5af6d31333d963e84
Jose V. Die <jodiera@upv.es>
2019-06-16 13:33:18 +0200

    Update and rename tema2_Basics.R to dates.R

--- dates.R
+++ dates.R
@@ -1,36 +1,3 @@
-## ========================================================================
-## Chapter 2. Fundamentos Basicos de R (I)
-## ========================================================================
-
-## Mi primer script
-## -----------------------------------------------------------------------
-# Load datasets library
-library(datasets)
-
-# Read data set 
-mydata = ChickWeight
-
-# Plot weight over diets
-boxplot(mydata$weight ~ mydata$Diet, 
-        xlab = "Diet", ylab = "chick weight (g)")
-
-# Overlay points
-colors = as.numeric(ChickWeight$Diet)
-points(mydata$weight ~ jitter(as.numeric(mydata$Diet)), 
-       col= alpha(colors, 0.4),
-       pch=19) 
-
-
-
-# =============================================================================
-## Directorio de trabajo
-# =============================================================================
-
-
-# =============================================================================
-## Built-in functions
-# =============================================================================
-
 
 # =============================================================================
 ## Fechas
